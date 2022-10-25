97465d7617d82495ccac2fa29f2128dbe913a4bd
ashtilawat23 <ashtilawat23@gmail.com>
2022-10-24 22:28:40 -0400

    fixed admin render bug

--- src/components/pages/Admin/Admin.js
+++ src/components/pages/Admin/Admin.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useOktaAuth } from '@okta/okta-react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import AdminHeader from '../../common/Admin/AdminHeader';
 // import AdminSideBar from '../../common/Admin/AdminSidebar';
 import StoryManager from './StoryManager/StoryManager';
@@ -10,24 +9,10 @@ const Admin = () => {
   const { isAuthenticated } = useOktaAuth();
 
   return (
-    isAuthenticated && (
-      <Router>
-        {/* <AdminHeader /> */}
-        <div className="main">
-          {/* <AdminSideBar /> */}
-          <div className="container">
-            <StoryManager />
-            <Switch>
-              <Route path="/admin/storymanager" component={StoryManager} />
-            </Switch>
-          </div>
-        </div>
-        <div>
-          {/* <ParentFooter layoutContainerCheck={'no-scroll'} /> */}
-          <ParentFooter />
-        </div>
-      </Router>
-    )
+    <>
+      <StoryManager />
+      <ParentFooter />
+    </>
   );
 };
 
